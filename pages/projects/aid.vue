d7a880ae6ca1f42efb3aa3981bca424c2a603f4d
crock <christopher.rock@student.uni-luebeck.de>
2023-04-11 18:56:17 +0200

    seo - robotstxt, sitemap

--- pages/projects/aid.vue
+++ pages/projects/aid.vue
@@ -133,7 +133,7 @@
             <span class="icon">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.16113 4.46875C5.58508 2.0448 9.44716 1.9355 12.0008 4.14085C14.5528 1.9355 18.4149 2.0448 20.8388 4.46875C23.2584 6.88836 23.3716 10.741 21.1785 13.2947L13.4142 21.0858C12.6686 21.8313 11.4809 21.8652 10.6952 21.1874L10.5858 21.0858L2.82141 13.2947C0.628282 10.741 0.741522 6.88836 3.16113 4.46875ZM4.57534 5.88296C2.86819 7.59011 2.81942 10.3276 4.42902 12.0937L4.57534 12.2469L12 19.6715L17.3026 14.3675L13.7677 10.8327L12.7071 11.8934C11.5355 13.0649 9.636 13.0649 8.46443 11.8934C7.29286 10.7218 7.29286 8.8223 8.46443 7.65073L10.5656 5.54823C8.85292 4.17713 6.37076 4.23993 4.7286 5.73663L4.57534 5.88296ZM13.0606 8.71139C13.4511 8.32086 14.0843 8.32086 14.4748 8.71139L18.7168 12.9533L19.4246 12.2469C21.1819 10.4896 21.1819 7.64032 19.4246 5.88296C17.7174 4.17581 14.9799 4.12704 13.2139 5.73663L13.0606 5.88296L9.87864 9.06494C9.51601 9.42757 9.49011 9.99942 9.80094 10.3919L9.87864 10.4792C10.2413 10.8418 10.8131 10.8677 11.2056 10.5569L11.2929 10.4792L13.0606 8.71139Z" fill="rgba(30,65,101,1)"></path></svg>            </span>
             <span>Honesty, not marketing</span>
-            <p>The simulator communicates not only possibilities but also functional limitations.</p>
+            <p>The simulator communicates not only possibilities but also functional limitations</p>
           </div>
 
           <div class="card">
@@ -166,7 +166,7 @@
           For this purpose, we have introduced two different types of explanations:
         </p>
         <ul>
-          <li><b>Local explanations</b> — Users can check out extreme points on the curve and get an insights for the expression of the data at the point</li>
+          <li><b>Local explanations</b> — Users can check out extreme points on the curve and get insights on the expression of the data at that point</li>
           <li><b>Global explanations</b> — An interactive visualization of the data flow conveys the influence of the inputs on the calculation process. Users can thus track step by step which changes certain inputs result in.</li>
         </ul>
       </section>
